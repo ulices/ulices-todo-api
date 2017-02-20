e1d70f8b40c267d91a5aea7a73f892abb0a01f43
ulices anibal barajas chavez <ubarajas@gopro.com>
2017-02-20 16:40:50 -0600

    Add POST for todos

--- server.js
+++ server.js
@@ -1,26 +1,15 @@
 var express = require('express');
+var bodyParser = require('body-parser');
+
 var app = express();
 var PORT = process.env.PORT || 3000;
-var todos = [{
-  id: 1,
-  description: 'Meet mom for lunch',
-  completed: false
-}, {
-  id: 2,
-  description: 'Go to market',
-  completed: false
-}, {
-  id: 3,
-  description: 'Feed my dogs',
-  completed: true
-}];
+var todos = [];
+var todoNextId = 1;
+
+app.use(bodyParser.json());
 
 app.get('/', function (req, res) {
   res.send('Todo API Root');
-});
-
-app.get('/todos', function (req, res) {
-   res.json(todos);
 });
 
 findTodoItem = function (todoId) {
@@ -32,6 +21,12 @@ findTodoItem = function (todoId) {
   return matchedTodo;
 };
 
+// GET all
+app.get('/todos', function (req, res) {
+   res.json(todos);
+});
+
+//GET by id
 app.get('/todos/:id', function (req, res) {
   var todoId = parseInt(req.params.id, 10);
   var matchedTodo = findTodoItem(todoId);
@@ -41,6 +36,16 @@ app.get('/todos/:id', function (req, res) {
   } else {
     res.status(404).send();
   }
+});
+
+//  POST
+app.post('/todos', function (req, res) {
+  var body = req.body;
+
+  body.id = todoNextId++;
+  todos.push(body);
+
+  res.json(body);
 });
 
 app.listen(PORT, function () {
